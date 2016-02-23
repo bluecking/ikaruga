813ffaf7d4ea0b48ff5ad2667aaf0deac440a9b3
Patrick Steinforth <patrick@steinforth.eu>
2016-02-23 20:00:58 +0100

    more stuff

--- src/xml/XmlTest.cpp
+++ src/xml/XmlTest.cpp
@@ -20,11 +20,58 @@ int main(int argc, char** argv)
     cout << "ID: " << m_xml.getId() << endl
         << "Levelname: " << m_xml.getLevelname() << endl
         << "Tileset: " << m_xml.getTileset() << endl
-        << "Background: " << m_xml.getBackground().filename << endl
-        << "Scrollspeed: " << m_xml.getBackground().scrollspeed << endl
+        << "Background - filename: " << m_xml.getBackground().filename << endl
+        << "Background - Scrollspeed: " << m_xml.getBackground().scrollspeed << endl
         << "Player - fileName: " << m_xml.getPlayer().filename << endl
         << "Player - frameWidth: " << m_xml.getPlayer().frameWidth << endl
         << "Player - frameHeight: " << m_xml.getPlayer().frameHeight << endl
         << "Player - positionY: " << m_xml.getPlayer().positionY << endl
         << "Player - stdWeapon: " << m_xml.getPlayer().stdWeapon << endl;
 }
+
+
+<background filename="background_40x40.png">
+                                       <scrollspeed>3</scrollspeed>
+</background>
+<player filename="player_combined_47x51.png">
+                 <frameWidth>18</frameWidth>
+<frameHeight>32</frameHeight>
+<positionY>40</positionY>
+<stdWeapon>LaserGun</stdWeapon>
+</player>
+<bot filename="enemys.png">
+              <frameWidth>18</frameWidth>
+<frameHeight>32</frameHeight>
+<tileID>1</tileID>
+<positionX>410</positionX>
+<positionY>40</positionY>
+<npc type="Bot">
+          <move function="sin">3</move>
+<fireRate>16</fireRate>
+<speed>-2</speed>
+<weapon type="laser">1</weapon>
+</npc>
+<color>red</color>
+</bot>
+<bot filename="enemys.png">
+              <frameWidth>18</frameWidth>
+<frameHeight>32</frameHeight>
+<tileID>5</tileID>
+<positionX>100</positionX>
+<positionY>50</positionY>
+<npc type="Boss">
+          <move function="sin">0</move>
+<fireRate>16</fireRate>
+<speed>-2</speed>
+<weapon type="laser">0</weapon>
+</npc>
+<color>blue</color>
+</bot>
+<item filename="powerups_20x20_transparent.png">
+               <frameWidth>18</frameWidth>
+<frameHeight>32</frameHeight>
+<positionX>40</positionX>
+<positionY>30</positionY>
+<type>laser</type>
+</item>
+</level>
