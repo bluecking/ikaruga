3ec692a418f009f16665da88c098cee522fd6376
Johan M. von Behren <johan@vonbehren.eu>
2016-02-23 11:17:55 +0100

    Changes player to ship and removes orientation change.

--- src/game/Actor.cpp
+++ src/game/Actor.cpp
@@ -8,6 +8,7 @@
 #include <SDL.h>
 
 #include <iostream>
+
 using std::cout;
 using std::endl;
 
@@ -16,188 +17,178 @@ using std::endl;
 namespace jumper
 {
 
-Actor::Actor(SDL_Renderer* renderer, std::string filename)
-	: AnimatedRenderable(renderer, filename)
-{
-	m_focus = false;
-	m_physicalProps.setPosition(Vector2f(100, 0));
-	m_startTicks = 0;
-	m_numFrames = 1;
-	m_type = ACTOR;
-}
-
-Actor::Actor(SDL_Renderer* renderer, SDL_Texture* texture, int frameWidth, int frameHeight, int numFrames)
-	: AnimatedRenderable(renderer, texture, frameWidth, frameHeight, numFrames)
-{
-	m_focus = false;
-	m_physicalProps.setPosition(Vector2f(100, 0));
-	m_startTicks = 0;
-	m_type = ACTOR;
-}
-
-void Actor::setPhysics(PlayerProperty p)
-{
-	m_physicalProps = p;
-}
-
-Actor::~Actor()
-{
-
-}
-
-float Actor::getElapsedTime()
-{
-	if(m_startTicks == 0)
-	{
-		m_startTicks = SDL_GetTicks();
-	}
-
-	Uint32 ticks = SDL_GetTicks();
-	float time =  (ticks - m_startTicks) / 1000.0;
-	m_startTicks = ticks;
-	return time;
-}
-
-void Actor::render()
-{
-    SDL_Rect target;
-
-    target.x = floor(m_physicalProps.position().x()) - m_camera.x();
-    target.y = floor(m_physicalProps.position().y()) - m_camera.y();
-    target.w = m_frameWidth;
-    target.h = m_frameHeight;
-
-    // Do not render if actor is outside frustrum
-    if(target.x + target.h > 0 && target.x + target.h < m_camera.w())
+    Actor::Actor(SDL_Renderer* renderer, std::string filename)
+            : AnimatedRenderable(renderer, filename)
     {
-    	SDL_RendererFlip flip;
-    	if(m_physicalProps.velocity().x() > 0)
-    	{
-    		flip = SDL_FLIP_HORIZONTAL;
-    	}
-    	else
-    	{
-    		flip = SDL_FLIP_NONE;
-    	}
-
-    	// Render current animation frame
-    	SDL_RenderCopyEx( getRenderer(), m_texture, &m_sourceRect, &target, 0, NULL, flip);
+        m_focus = false;
+        m_physicalProps.setPosition(Vector2f(100, 0));
+        m_startTicks = 0;
+        m_numFrames = 1;
+        m_type = ACTOR;
     }
 
-}
+    Actor::Actor(SDL_Renderer* renderer, SDL_Texture* texture, int frameWidth, int frameHeight, int numFrames)
+            : AnimatedRenderable(renderer, texture, frameWidth, frameHeight, numFrames)
+    {
+        m_focus = false;
+        m_physicalProps.setPosition(Vector2f(100, 0));
+        m_startTicks = 0;
+        m_type = ACTOR;
+    }
 
-void Actor::setPosition(Vector2f pos)
-{
-    m_physicalProps.position() = pos;
-}
+    void Actor::setPhysics(PlayerProperty p)
+    {
+        m_physicalProps = p;
+    }
 
-PlayerProperty& Actor::physics()
-{
-    return m_physicalProps;
-}
+    Actor::~Actor()
+    {
 
-Vector2f Actor:: position()
-{
-	return m_physicalProps.position();
-}
+    }
 
-void Actor::resolveCollision(Actor& other)
-{
-	SDL_Rect myRect;
-	myRect.x = position().x();
-	myRect.y = position().y();
-	myRect.w = w();
-	myRect.h = h();
+    float Actor::getElapsedTime()
+    {
+        if (m_startTicks == 0)
+        {
+            m_startTicks = SDL_GetTicks();
+        }
 
-	SDL_Rect otherRect;
-	otherRect.x = other.position().x();
-	otherRect.y = other.position().y();
-	otherRect.w = other.w();
-	otherRect.h = other.h();
+        Uint32 ticks = SDL_GetTicks();
+        float time = (ticks - m_startTicks) / 1000.0;
+        m_startTicks = ticks;
+        return time;
+    }
 
-	SDL_Rect intersection;
-	SDL_IntersectRect(&myRect, &otherRect, &intersection);
+    void Actor::render()
+    {
+        SDL_Rect target;
 
-	//cout << intersection.w << endl;
-	if(intersection.h > 0 && intersection.w > 0)
-	{
-		Vector2f tmp = position();
-		Vector2f tmp_v = m_physicalProps.velocity();
-		tmp_v.setY(0);
+        target.x = floor(m_physicalProps.position().x()) - m_camera.x();
+        target.y = floor(m_physicalProps.position().y()) - m_camera.y();
+        target.w = m_frameWidth;
+        target.h = m_frameHeight;
 
-		if(m_physicalProps.velocity().y() > 0)
-		{
-			tmp.setY(position().y() - intersection.h);
-		}
-		else
-		{
-			tmp.setY(position().y() + intersection.h);
-		}
-		setPosition(tmp);
-		m_physicalProps.setVelocity(tmp_v);
-	}
-}
+        // Do not render if actor is outside frustrum
+        if (target.x + target.h > 0 && target.x + target.h < m_camera.w())
+        {
+            // Render current animation frame
+            SDL_RenderCopyEx(getRenderer(), m_texture, &m_sourceRect, &target, 0, NULL, SDL_FLIP_NONE);
+        }
 
-Collision Actor::getCollision(Actor& other)
-{
-	Collision c;
+    }
 
-	// Check for collision
-	Vector2f v = m_physicalProps.velocity();
+    void Actor::setPosition(Vector2f pos)
+    {
+        m_physicalProps.position() = pos;
+    }
 
-	SDL_Rect myRect;
-	myRect.x = position().x();
-	myRect.y = position().y();
-	myRect.w = w();
-	myRect.h = h();
+    PlayerProperty& Actor::physics()
+    {
+        return m_physicalProps;
+    }
 
-	SDL_Rect otherRect;
-	otherRect.x = other.position().x();
-	otherRect.y = other.position().y();
-	otherRect.w = other.w();
-	otherRect.h = other.h();
+    Vector2f Actor::position()
+    {
+        return m_physicalProps.position();
+    }
 
-	SDL_Rect intersection;
-	SDL_IntersectRect(&myRect, &otherRect, &intersection);
+    void Actor::resolveCollision(Actor& other)
+    {
+        SDL_Rect myRect;
+        myRect.x = position().x();
+        myRect.y = position().y();
+        myRect.w = w();
+        myRect.h = h();
 
-	if(fabs(intersection.w) < otherRect.w && intersection.h > 0)
-	{
-		if(v.y() > 0)
-		{
-			if(intersection.h < otherRect.h / 2)
-			{
-				c.setType(DOWN);
-			}
-			else
-			{
-				c.setType(BOOM);
-			}
-		}
-		else
-		{
-			if(intersection.h < otherRect.h / 2)
-			{
-				c.setType(UP);
-			}
-			else
-			{
-				c.setType(BOOM);
-			}
-		}
-	}
+        SDL_Rect otherRect;
+        otherRect.x = other.position().x();
+        otherRect.y = other.position().y();
+        otherRect.w = other.w();
+        otherRect.h = other.h();
 
-	return c;
-}
+        SDL_Rect intersection;
+        SDL_IntersectRect(&myRect, &otherRect, &intersection);
 
-void jumper::Actor::setFocus(bool focus)
-{
-	m_focus = focus;
-}
+        //cout << intersection.w << endl;
+        if (intersection.h > 0 && intersection.w > 0)
+        {
+            Vector2f tmp = position();
+            Vector2f tmp_v = m_physicalProps.velocity();
+            tmp_v.setY(0);
 
-bool jumper::Actor::hasFocus()
-{
-	return m_focus;
-}
+            if (m_physicalProps.velocity().y() > 0)
+            {
+                tmp.setY(position().y() - intersection.h);
+            }
+            else
+            {
+                tmp.setY(position().y() + intersection.h);
+            }
+            setPosition(tmp);
+            m_physicalProps.setVelocity(tmp_v);
+        }
+    }
+
+    Collision Actor::getCollision(Actor& other)
+    {
+        Collision c;
+
+        // Check for collision
+        Vector2f v = m_physicalProps.velocity();
+
+        SDL_Rect myRect;
+        myRect.x = position().x();
+        myRect.y = position().y();
+        myRect.w = w();
+        myRect.h = h();
+
+        SDL_Rect otherRect;
+        otherRect.x = other.position().x();
+        otherRect.y = other.position().y();
+        otherRect.w = other.w();
+        otherRect.h = other.h();
+
+        SDL_Rect intersection;
+        SDL_IntersectRect(&myRect, &otherRect, &intersection);
+
+        if (fabs(intersection.w) < otherRect.w && intersection.h > 0)
+        {
+            if (v.y() > 0)
+            {
+                if (intersection.h < otherRect.h / 2)
+                {
+                    c.setType(DOWN);
+                }
+                else
+                {
+                    c.setType(BOOM);
+                }
+            }
+            else
+            {
+                if (intersection.h < otherRect.h / 2)
+                {
+                    c.setType(UP);
+                }
+                else
+                {
+                    c.setType(BOOM);
+                }
+            }
+        }
+
+        return c;
+    }
+
+    void jumper::Actor::setFocus(bool focus)
+    {
+        m_focus = focus;
+    }
+
+    bool jumper::Actor::hasFocus()
+    {
+        return m_focus;
+    }
 
 
 } /* namespace jumper */
--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <algorithm>
 #include <set>
+
 using std::set;
 using std::cout;
 using std::endl;
@@ -19,221 +20,208 @@ using std::endl;
 namespace jumper
 {
 
-Game::Game(MainWindow* mainWindow)
-{
-	m_player = 0;
-	m_level = 0;
-	m_layer = 0;
-	m_renderer = mainWindow->getRenderer();
-	m_scoreBoard = 0;
-
-	m_windowWidth = mainWindow->w();
-	m_windowHeight = mainWindow->h();
-
-	m_started = false;
-    m_startTicks = 0;
-
-	SDL_SetRenderDrawColor(m_renderer, 0, 102, 204, 255);
-}
-
-Game::~Game()
-{
-	// TODO Auto-generated destructor stub
-}
-
-void Game::setPlayer(Player* player)
-{
-	m_player = player;
-	m_actors.push_back(player);
-	m_renderables.push_back(player);
-}
-
-void Game::setLevel(Level* level)
-{
-	m_level = level;
-	m_renderables.push_back(level);
-}
-
-void Game::addActor(Actor* actor)
-{
-	m_actors.push_back(actor);
-	m_renderables.push_back(actor);
-}
-
-void Game::update(const Uint8* &currentKeyStates)
-{
-	if(m_started)
-	{
-		// Reset forces and jump flags
-        Vector2f moveForce(0.0, 0.0);
-
-		if( currentKeyStates[ SDL_SCANCODE_UP ] )
-		{
-            moveForce.setY(-800.0);
-		}
-		if( currentKeyStates[ SDL_SCANCODE_DOWN ] )
-		{
-            moveForce.setY(800.0);
-		}
-		if( currentKeyStates[ SDL_SCANCODE_LEFT ] )
-		{
-            moveForce.setX(-800.0);
-		}
-		if( currentKeyStates[ SDL_SCANCODE_RIGHT ] )
-		{
-            moveForce.setX(800.0);
-		}
-
-        m_player->physics().setMoveForce(moveForce);
-
-		moveActors();
-		checkPlayerCollision();
-
-		SDL_RenderClear(m_renderer);
-
-		if(m_layer)
-		{
-			m_layer->render();
-		}
-
-		if(m_scoreBoard)
-		{
-			m_scoreBoard->setScore(m_player->physics().position().x());
-			m_scoreBoard->render();
-		}
-
-		for(size_t i = 0; i < m_renderables.size(); i++)
-		{
-			m_renderables[i]->render();
-		}
-
-		updateCameraPosition();
-
-        scrollHorizontal();
-
-		// Update screen
-		SDL_RenderPresent(m_renderer);
-	}
-}
-
-void Game::removeActor(Actor* a)
-{
-	auto it = std::find(m_actors.begin(), m_actors.end(), a);
-	m_actors.erase(it);
-
-	auto it2 = std::find(m_renderables.begin(), m_renderables.end(), a);
-	m_renderables.erase(it2);
-}
-
-void Game::checkPlayerCollision()
-{
-	set<Actor*> to_remove;
-	KillAnimation* anim = 0;
-	for(auto it = m_actors.begin(); it != m_actors.end(); it++)
-	{
-		Actor* a = *it;
-
-		// Check for self collision
-		if(a != m_player)
-		{
-			Collision c = m_player->getCollision(*a);
-
-			// Simple items can be collected on the fly
-			if(a->type() == ITEM && c.type() != NONE)
-			{
-				to_remove.insert(a);
-			}
-
-			// If an collection with an enemy occured, check
-			// who killed whom (Player only can kill enemies
-			// when falling down.
-			if(a->type() == ENEMY  && c.type() != NONE)
-			{
-				if(c.type() == BOOM)
-				{
-					cout << "PLAYER KILLED" << endl;
-				}
-				else if(c.type() == DOWN)
-				{
-					to_remove.insert(a);
-					anim = new KillAnimation(a);
-				}
-			}
-
-			if(a->type() == PUZZLEBOX)
-			{
-				PuzzleBox* b = static_cast<PuzzleBox*>(a);
-				m_player->resolveCollision(*a);
-				if(c.type() == UP)
-				{
-					b->setHit(true);
-				}
-			}
-		}
-	}
-
-	// Remove actors that were killed in this loop. We have to
-	// store them separately because otherwise we would corrupt
-	// to loop structure
-	for(auto i = to_remove.begin(); i != to_remove.end(); i++)
-	{
-		removeActor(*i);
-	}
-
-	if(anim)
-	{
-		m_renderables.push_back(anim);
-	}
-}
-
-void Game::moveActors()
-{
-	for(auto it = m_actors.begin(); it != m_actors.end(); it++)
-	{
-		(*it)->move(*m_level);
-	}
-}
-
-void Game::start()
-{
-	m_started = true;
-}
-
-void Game::updateCameraPosition()
-{
-	/*if(m_player->position().x() > m_windowWidth / 2)
-	{
-		Vector2i position(m_player->position().x() - m_windowWidth / 2, 0);
-		Renderable::m_camera.move(position);
-	}*/
-
-
-}
-
-void Game::scrollHorizontal()
-{
-    float dt = getElapsedTime();
-
-    if(dt > 0)
+    Game::Game(MainWindow* mainWindow)
     {
-        Vector2f scrollOffset(m_level->physics().getScrollingSpeed() * dt);
-        m_player->setPosition(m_player->position() + scrollOffset);
+        m_player = 0;
+        m_level = 0;
+        m_layer = 0;
+        m_renderer = mainWindow->getRenderer();
+        m_scoreBoard = 0;
 
-        Renderable::m_camera.move(Renderable::m_camera.position() + scrollOffset);
-    }
-}
+        m_windowWidth = mainWindow->w();
+        m_windowHeight = mainWindow->h();
 
-float Game::getElapsedTime()
-{
-    if(m_startTicks == 0)
-    {
-        m_startTicks = SDL_GetTicks();
+        m_started = false;
+        m_startTicks = 0;
+
+        SDL_SetRenderDrawColor(m_renderer, 0, 102, 204, 255);
     }
 
-    Uint32 ticks = SDL_GetTicks();
-    float time =  (ticks - m_startTicks) / 1000.0;
-    m_startTicks = ticks;
-    return time;
-}
+    Game::~Game()
+    {
+        // TODO Auto-generated destructor stub
+    }
+
+    void Game::setPlayer(Player* player)
+    {
+        m_player = player;
+        m_actors.push_back(player);
+        m_renderables.push_back(player);
+    }
+
+    void Game::setLevel(Level* level)
+    {
+        m_level = level;
+        m_renderables.push_back(level);
+    }
+
+    void Game::addActor(Actor* actor)
+    {
+        m_actors.push_back(actor);
+        m_renderables.push_back(actor);
+    }
+
+    void Game::update(const Uint8*& currentKeyStates)
+    {
+        if (m_started)
+        {
+            // Reset forces and jump flags
+            Vector2f moveForce(0.0, 0.0);
+
+            if (currentKeyStates[SDL_SCANCODE_UP])
+            {
+                moveForce.setY(-800.0);
+            }
+            if (currentKeyStates[SDL_SCANCODE_DOWN])
+            {
+                moveForce.setY(800.0);
+            }
+            if (currentKeyStates[SDL_SCANCODE_LEFT])
+            {
+                moveForce.setX(-800.0);
+            }
+            if (currentKeyStates[SDL_SCANCODE_RIGHT])
+            {
+                moveForce.setX(800.0);
+            }
+
+            m_player->physics().setMoveForce(moveForce);
+
+            moveActors();
+            checkPlayerCollision();
+
+            SDL_RenderClear(m_renderer);
+
+            if (m_layer)
+            {
+                m_layer->render();
+            }
+
+            if (m_scoreBoard)
+            {
+                m_scoreBoard->setScore(m_player->physics().position().x());
+                m_scoreBoard->render();
+            }
+
+            for (size_t i = 0; i < m_renderables.size(); i++)
+            {
+                m_renderables[i]->render();
+            }
+
+            scrollHorizontal();
+
+            // Update screen
+            SDL_RenderPresent(m_renderer);
+        }
+    }
+
+    void Game::removeActor(Actor* a)
+    {
+        auto it = std::find(m_actors.begin(), m_actors.end(), a);
+        m_actors.erase(it);
+
+        auto it2 = std::find(m_renderables.begin(), m_renderables.end(), a);
+        m_renderables.erase(it2);
+    }
+
+    void Game::checkPlayerCollision()
+    {
+        set<Actor*> to_remove;
+        KillAnimation* anim = 0;
+        for (auto it = m_actors.begin(); it != m_actors.end(); it++)
+        {
+            Actor* a = *it;
+
+            // Check for self collision
+            if (a != m_player)
+            {
+                Collision c = m_player->getCollision(*a);
+
+                // Simple items can be collected on the fly
+                if (a->type() == ITEM && c.type() != NONE)
+                {
+                    to_remove.insert(a);
+                }
+
+                // If an collection with an enemy occured, check
+                // who killed whom (Player only can kill enemies
+                // when falling down.
+                if (a->type() == ENEMY && c.type() != NONE)
+                {
+                    if (c.type() == BOOM)
+                    {
+                        cout << "PLAYER KILLED" << endl;
+                    }
+                    else if (c.type() == DOWN)
+                    {
+                        to_remove.insert(a);
+                        anim = new KillAnimation(a);
+                    }
+                }
+
+                if (a->type() == PUZZLEBOX)
+                {
+                    PuzzleBox* b = static_cast<PuzzleBox*>(a);
+                    m_player->resolveCollision(*a);
+                    if (c.type() == UP)
+                    {
+                        b->setHit(true);
+                    }
+                }
+            }
+        }
+
+        // Remove actors that were killed in this loop. We have to
+        // store them separately because otherwise we would corrupt
+        // to loop structure
+        for (auto i = to_remove.begin(); i != to_remove.end(); i++)
+        {
+            removeActor(*i);
+        }
+
+        if (anim)
+        {
+            m_renderables.push_back(anim);
+        }
+    }
+
+    void Game::moveActors()
+    {
+        for (auto it = m_actors.begin(); it != m_actors.end(); it++)
+        {
+            (*it)->move(*m_level);
+        }
+    }
+
+    void Game::start()
+    {
+        m_started = true;
+    }
+
+    void Game::scrollHorizontal()
+    {
+        float dt = getElapsedTime();
+
+        if (dt > 0)
+        {
+            Vector2f scrollOffset(m_level->physics().getScrollingSpeed() * dt);
+            m_player->setPosition(m_player->position() + scrollOffset);
+
+            Renderable::m_camera.move(Renderable::m_camera.position() + scrollOffset);
+        }
+    }
+
+    float Game::getElapsedTime()
+    {
+        if (m_startTicks == 0)
+        {
+            m_startTicks = SDL_GetTicks();
+        }
+
+        Uint32 ticks = SDL_GetTicks();
+        float time = (ticks - m_startTicks) / 1000.0;
+        m_startTicks = ticks;
+        return time;
+    }
 
 } /* namespace jumper */
