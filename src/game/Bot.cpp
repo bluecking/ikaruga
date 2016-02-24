5e7369f7e6b864aa529edee50508335d6bd978b0
Johan M. von Behren <johan@vonbehren.eu>
2016-02-24 11:07:52 +0100

    Applies correct code style

--- src/game/Bot.cpp
+++ src/game/Bot.cpp
@@ -8,83 +8,75 @@
 #include "Bot.hpp"
 
 #include <iostream>
+
 using std::cout;
 using std::endl;
 
 namespace jumper
 {
+    Bot::Bot(SDL_Renderer* renderer, SDL_Texture* texture, int frameWidth, int frameHeight, int numFrames)
+            : Actor(renderer, texture, frameWidth, frameHeight, numFrames)
+    {
+        m_physicalProps.setMoveForce(Vector2f(0, 0));
+        m_physicalProps.setMaxRunVelocity(50);
+    }
 
-Bot::Bot(SDL_Renderer* renderer, std::string filename)
-	: Actor(renderer, filename), m_bouncePos(0)
-{
-	m_physicalProps.setMoveForce(Vector2f(0,0));
-	m_physicalProps.setMaxRunVelocity(50);
-}
+    Bot::~Bot()
+    {
+        // TODO Auto-generated destructor stub
+    }
 
-Bot::Bot(SDL_Renderer* renderer, SDL_Texture* texture, int frameWidth, int frameHeight, int numFrames)
-  	: Actor(renderer, texture, frameWidth, frameHeight, numFrames)
-{
-	m_physicalProps.setMoveForce(Vector2f(0,0));
-	m_physicalProps.setMaxRunVelocity(50);
-}
+    void Bot::move(Level& level)
+    {
+        nextFrame();
+        float dt = getElapsedTime();
+        if (dt > 0)
+        {
+            Collision c = level.resolveCollision(this);
+            if (c.delta().x() > 5)
+            {
+                bounce();
+            }
 
-Bot::~Bot()
-{
-	// TODO Auto-generated destructor stub
-}
+            if (fabs(physics().moveForce().x()) < 2)
+            {
+                physics().setMoveForce(Vector2f(100, 0));
+            }
 
-void Bot::move(Level& level)
-{
-	nextFrame();
-	float dt = getElapsedTime();
-	if(dt > 0)
-	{
-		Collision c = level.resolveCollision(this);
-		if(c.delta().x() > 5)
-		{
-			bounce();
-		}
+            Vector2f d_move;
 
-		if(fabs(physics().moveForce().x()) < 2)
-		{
-			physics().setMoveForce(Vector2f(100, 0));
-		}
+            d_move = (physics().moveForce() * dt);
 
-		Vector2f d_move;
+            // Update velocity
+            physics().setVelocity(physics().velocity() + d_move);
 
-		d_move = (physics().moveForce() * dt);
+            // Damp velocity according to extrinsic level damping
+            physics().setVelocity(physics().velocity() * level.physics().damping());
 
-		// Update velocity
-		physics().setVelocity(physics().velocity() + d_move);
+            if (physics().velocity().x() > physics().maxRunVelocity() * dt)
+            {
+                physics().setVelocity(Vector2f(physics().maxRunVelocity() * dt,
+                                               physics().velocity().y()));
+            }
 
-		// Damp velocity according to extrinsic level damping
-		physics().setVelocity(physics().velocity() * level.physics().damping());
+            if (physics().velocity().x() < -physics().maxRunVelocity() * dt)
+            {
+                physics().setVelocity(Vector2f(-physics().maxRunVelocity() * dt,
+                                               physics().velocity().y()));
+            }
 
-		if(physics().velocity().x() > physics().maxRunVelocity() * dt)
-		{
-			physics().setVelocity(Vector2f(physics().maxRunVelocity() * dt,
-					physics().velocity().y()));
-		}
+            // Set new player position
+            physics().setPosition(physics().position() + physics().velocity());
 
-		if(physics().velocity().x() < -physics().maxRunVelocity() * dt)
-		{
-			physics().setVelocity(Vector2f(-physics().maxRunVelocity() * dt,
-					physics().velocity().y()));
-		}
+        }
+    }
 
-		// Set new player position
-		physics().setPosition(physics().position() + physics().velocity());
-
-	}
-}
-
-void Bot::bounce()
-{
-	if(fabs(physics().position().x() - m_bouncePos.x()) > 5)
-	{
-		physics().setMoveForce(physics().moveForce() * -1);
-		m_bouncePos = physics().position();
-	}
-}
-
+    void Bot::bounce()
+    {
+        if (fabs(physics().position().x() - m_bouncePos.x()) > 5)
+        {
+            physics().setMoveForce(physics().moveForce() * -1);
+            m_bouncePos = physics().position();
+        }
+    }
 } /* namespace jumper */
--- src/game/Bot.hpp
+++ src/game/Bot.hpp
@@ -13,48 +13,39 @@
 
 namespace jumper
 {
+    /**
+     * @brief An actor that moves in a level according to a predefined movement pattern.
+     * 		  The default implementation switches x-direction when it hits an object.
+     */
+    class Bot : public Actor
+    {
+    public:
 
-/**
- * @brief An actor that moves in a level according to a predefined movement pattern.
- * 		  The default implementation switches x-direction when it hits an object.
- */
-class Bot : public Actor
-{
-public:
+        /**
+         * @brief	Constructs a bot from given renderer, texture
+         * 			and frame information.
+         * @param renderer		A pointer to a SDL renderer struct
+         * @param texture		A pointer to valid SDL_Texture struct
+         * @param frameWidth	The width of the frames within the texture
+         * @param frameHeight	The height of the frames within the texture
+         * @param numFrames		The number of frames in the texture
+         */
+        Bot(SDL_Renderer* renderer, SDL_Texture* texture, int frameWidth, int frameHeight, int numFrames);
 
+        virtual ~Bot();
 
-	/***
-	 * @brief 	Constructs a bot from the given \ref filename
-	 * 			for the internal \ref renderer
-	 * @param renderer		A pointer to a SDL renderer struct
-	 * @param filename		A filename with animation definitions
-	 */
-	Bot(SDL_Renderer* renderer, std::string filename);
+        /// Moves the bot in the given \ref level
+        virtual void move(Level& level);
 
-	/**
-	 * @brief	Constructs a bot from given renderer, texture
-	 * 			and frame information.
-	 * @param renderer		A pointer to a SDL renderer struct
-	 * @param texture		A pointer to valid SDL_Texture struct
-	 * @param frameWidth	The width of the frames within the texture
-	 * @param frameHeight	The height of the frames within the texture
-	 * @param numFrames		The number of frames in the texture
-	 */
-	Bot(SDL_Renderer* renderer, SDL_Texture* texture, int frameWidth, int frameHeight, int numFrames);
-	virtual ~Bot();
+    private:
 
-	/// Moves the bot in the given \ref level
-	virtual void move(Level& level);
+        /// Changes x-direction when it hits an object
+        void bounce();
 
-private:
-
-	/// Changes x-direction when it hits an object
-	void bounce();
-
-	/// The last position where a collision occured.
-	/// Used the prevent continuous flipping.
-	Vector2f m_bouncePos;
-};
+        /// The last position where a collision occured.
+        /// Used the prevent continuous flipping.
+        Vector2f m_bouncePos;
+    };
 
 } /* namespace jumper */
 
