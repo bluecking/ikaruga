dcd44c3edeba5709bcfd292c1c2991809fb5d090
Sven Kleine-Albers <skleinealber@uos.de>
2016-02-25 11:36:44 +0100

    removing warnings, reformating code

--- src/xml/XML.cpp
+++ src/xml/XML.cpp
@@ -64,78 +64,76 @@ void XML::load()
     try
     {
         BOOST_FOREACH(const ptree::value_type& v, pt.get_child("level"))
-                    {
+            {
+                if (v.first == "id")
+                {
+                    m_id = v.second.get<int>("");
+                    m_requiredAttributes["id"]++;
+                }
+                else if (v.first == "name")
+                {
+                    m_levelname = v.second.get<string>("");
+                    m_requiredAttributes["name"]++;
+                }
+                else if (v.first == "tileset")
+                {
+                    m_tileset = v.second.get<string>("<xmlattr>.filename");
+                    m_requiredAttributes["tileset"]++;
+                }
+                else if (v.first == "background")
+                {
+                    m_background.filename = v.second.get<string>("<xmlattr>.filename");
+                    m_background.scrollspeed = v.second.get<int>("scrollspeed");
+                    m_requiredAttributes["background"]++;
+                }
+                else if (v.first == "player")
+                {
+                    m_player.filename = v.second.get<string>("<xmlattr>.filename");
+                    m_player.frameWidth = v.second.get<int>("frameWidth");
+                    m_player.frameHeight = v.second.get<int>("frameHeight");
+                    m_player.positionY = v.second.get<int>("positionY");
+                    m_player.stdWeapon = v.second.get<string>("stdWeapon");
+                    m_requiredAttributes["player"]++;
+                }
+                else if (v.first == "bot")
+                {
+                    Bot bot;
+                    bot.filename = v.second.get<string>("<xmlattr>.filename");
+                    bot.frameWidth = v.second.get<int>("frameWidth");
+                    bot.frameHeight = v.second.get<int>("frameHeight");
+                    bot.tileID = v.second.get<int>("tileID");
+                    bot.positionX = v.second.get<int>("positionX");
+                    bot.positionY = v.second.get<int>("positionY");
+                    bot.color = v.second.get<string>("color");
 
-                        if (v.first == "id")
-                        {
-                            m_id = v.second.get<int>("");
-                            m_requiredAttributes["id"]++;
-                        }
-                        else if (v.first == "name")
-                        {
-                            m_levelname = v.second.get<string>("");
-                            m_requiredAttributes["name"]++;
-                        }
-                        else if (v.first == "tileset")
-                        {
-                            m_tileset = v.second.get<string>("<xmlattr>.filename");
-                            m_requiredAttributes["tileset"]++;
-                        }
-                        else if (v.first == "background")
-                        {
-                            m_background.filename = v.second.get<string>("<xmlattr>.filename");
-                            m_background.scrollspeed = v.second.get<int>("scrollspeed");
-                            m_requiredAttributes["background"]++;
-                        }
-                        else if (v.first == "player")
-                        {
-                            m_player.filename = v.second.get<string>("<xmlattr>.filename");
-                            m_player.frameWidth = v.second.get<int>("frameWidth");
-                            m_player.frameHeight = v.second.get<int>("frameHeight");
-                            m_player.positionY = v.second.get<int>("positionY");
-                            m_player.stdWeapon = v.second.get<string>("stdWeapon");
-                            m_requiredAttributes["player"]++;
-                        }
-                        else if (v.first == "bot")
-                        {
-                            Bot bot;
-                            bot.filename = v.second.get<string>("<xmlattr>.filename");
-                            bot.frameWidth = v.second.get<int>("frameWidth");
-                            bot.frameHeight = v.second.get<int>("frameHeight");
-                            bot.tileID = v.second.get<int>("tileID");
-                            bot.positionX = v.second.get<int>("positionX");
-                            bot.positionY = v.second.get<int>("positionY");
-                            bot.color = v.second.get<string>("color");
-
-                            /* Get data from child node NPC */
-                            NPC npc;
-                            npc.type = v.second.get_child("npc").get<string>("<xmlattr>.type");
-                            npc.move_function = v.second.get_child("npc").get_child("move").get<string>(
-                                    "<xmlattr>.function");
-                            npc.move_value = v.second.get_child("npc").get < signed int > ("move");
-                            npc.fireRate = v.second.get_child("npc").get < unsigned int > ("fireRate");
-                            npc.speed = v.second.get_child("npc").get < signed int > ("speed");
-                            npc.weapon_type = v.second.get_child("npc").get_child("weapon").get<string>("<xmlattr>.type");
-                            npc.weapon_level = v.second.get_child("npc").get < unsigned int > ("weapon");
-                            bot.npc = npc;
-                            m_bots.push_back(bot);
-                        }
-                        else if (v.first == "item")
-                        {
-                            Item i;
-                            i.filename = v.second.get<string>("<xmlattr>.filename");
-                            i.frameWidth = v.second.get<int>("frameWidth");
-                            i.frameHeight = v.second.get<int>("frameHeight");
-                            i.positionX = v.second.get<int>("positionX");
-                            i.positionY = v.second.get<int>("positionY");
-                            i.type = v.second.get<string>("type");
-                            m_items.push_back(i);
-                        }
-                        else
-                        {
-                            throw std::domain_error("Found unknown xml tag " + v.first + " on first child layer below level.");
-                        }
-                    }
+                    /* Get data from child node NPC */
+                    NPC npc;
+                    npc.type = v.second.get_child("npc").get<string>("<xmlattr>.type");
+                    npc.move_function = v.second.get_child("npc").get_child("move").get<string>("<xmlattr>.function");
+                    npc.move_value = v.second.get_child("npc").get <signed(int)> ("move");
+                    npc.fireRate = v.second.get_child("npc").get <unsigned(int)> ("fireRate");
+                    npc.speed = v.second.get_child("npc").get <signed(int)> ("speed");
+                    npc.weapon_type = v.second.get_child("npc").get_child("weapon").get<string>("<xmlattr>.type");
+                    npc.weapon_level = v.second.get_child("npc").get <unsigned(int)> ("weapon");
+                    bot.npc = npc;
+                    m_bots.push_back(bot);
+                }
+                else if (v.first == "item")
+                {
+                    Item i;
+                    i.filename = v.second.get<string>("<xmlattr>.filename");
+                    i.frameWidth = v.second.get<int>("frameWidth");
+                    i.frameHeight = v.second.get<int>("frameHeight");
+                    i.positionX = v.second.get<int>("positionX");
+                    i.positionY = v.second.get<int>("positionY");
+                    i.type = v.second.get<string>("type");
+                    m_items.push_back(i);
+                }
+                else
+                {
+                    throw std::domain_error("Found unknown xml tag " + v.first + " on first child layer below level.");
+                }
+            }
     }
     catch (boost::exception const& e)
     {
@@ -158,7 +156,6 @@ void XML::load()
 
 void XML::save()
 {
-    //TODO implement and exception handling
     ptree root;
     ptree level;
     ptree tileset;
