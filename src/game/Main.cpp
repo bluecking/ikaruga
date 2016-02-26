100c659c9dd5cfa9e10ad2f5068d74b3ec30c9f8
Marius <mbrockmo@uos.de>
2016-02-26 16:30:54 +0100

    merge versuche der 1

--- src/game/Main.cpp
+++ src/game/Main.cpp
@@ -22,15 +22,15 @@ using std::endl;
 
 void getPlayerProperty(XML::Player player, PlayerProperty& p)
 {
-	int pos_x = player.positionX;
-	int pos_y = player.positionY;
-	float moveForceX =  player.moveForceX *1.0;
-	float moveForceY = player.moveForceY *1.0;
-	float maxVelRun = player.maxVel *1.0;
+    int pos_x = player.positionX;
+    int pos_y = player.positionY;
+    float moveForceX = player.moveForceX * 1.0;
+    float moveForceY = player.moveForceY * 1.0;
+    float maxVelRun = player.maxVel * 1.0;
 
-	p.setPosition(Vector2f(pos_x, pos_y));
-	p.setMoveForce(Vector2f(moveForceX, moveForceY));
-	p.setMaxRunVelocity(maxVelRun);
+    p.setPosition(Vector2f(pos_x, pos_y));
+    p.setMoveForce(Vector2f(moveForceX, moveForceY));
+    p.setMaxRunVelocity(maxVelRun);
 }
 
 
@@ -38,7 +38,7 @@ void getBotProperty(XML::LevelBot bot, PlayerProperty& p)
 {
     int pos_x = bot.positionX;
     int pos_y = bot.positionY;
-    float moveForceX =  1.0;
+    float moveForceX = 1.0;
     float moveForceY = 1.0;
     float maxVelRun = 1.0;
 
@@ -48,13 +48,12 @@ void getBotProperty(XML::LevelBot bot, PlayerProperty& p)
 }
 
 
-
 void setupGame(string filename, MainWindow* w, Game* game)
 {
-	//<alter Teil>
-	 std::size_t found = filename.find_last_of("/\\");
-	 string path = filename.substr(0,found);
-	//</alter Teil>
+    //<alter Teil>
+    std::size_t found = filename.find_last_of("/\\");
+    string path = filename.substr(0, found);
+    //</alter Teil>
 
 
     //<mein teil>
@@ -65,8 +64,8 @@ void setupGame(string filename, MainWindow* w, Game* game)
 
     //create Level
     string xpath = xml.getTileset();
-    Level* level = new Level(w->getRenderer(),path +"/" + xpath);
-	game->setLevel(level);
+    Level* level = new Level(w->getRenderer(), path + "/" + xpath);
+    game->setLevel(level);
 
 
 
@@ -74,19 +73,19 @@ void setupGame(string filename, MainWindow* w, Game* game)
 
 
     //create Background layer
-    XML::Background background  = xml.getBackground();
-    xpath                       = background.filename;
-    SDL_Texture* texture       = TextureFactory::instance(w->getRenderer()).getTexture(path + "/" + xpath);
-    float scrollspeed           = background.scrollspeed * 1.0;
-    TexturedLayer* layer       = new TexturedLayer(w->getRenderer(), texture, game->getLevel()->tileHeight());
+    XML::Background background = xml.getBackground();
+    xpath = background.filename;
+    SDL_Texture* texture = TextureFactory::instance(w->getRenderer()).getTexture(path + "/" + xpath);
+    float scrollspeed = background.scrollspeed * 1.0;
+    TexturedLayer* layer = new TexturedLayer(w->getRenderer(), texture, game->getLevel()->tileHeight());
     layer->setScrollSpeed(scrollspeed);
     game->setLayer(layer);
 
 
-    XML::Player xplayer  = xml.getPlayer();
-    xpath               = xplayer.filename;
-    texture       = TextureFactory::instance(w->getRenderer()).getTexture(path + "/" + xpath);
-    Player* player = new Player(w->getRenderer(), texture,xplayer.frameWidth, xplayer.frameHeight, xplayer.numFrames);
+    XML::Player xplayer = xml.getPlayer();
+    xpath = xplayer.filename;
+    texture = TextureFactory::instance(w->getRenderer()).getTexture(path + "/" + xpath);
+    Player* player = new Player(w->getRenderer(), texture, xplayer.frameWidth, xplayer.frameHeight, xplayer.numFrames);
 
     // set weapon
 
@@ -95,8 +94,11 @@ void setupGame(string filename, MainWindow* w, Game* game)
     Vector2f* weaponOffset = new Vector2f(25, 18.5f);
     Vector2f* projectileColorOffset = new Vector2f(6, 0);
     float coolDown = 0.2f;
-    SDL_Texture* weaponTexture = TextureFactory::instance(w->getRenderer()).getTexture(path + "/../images/laser_shot.png");
-    player->setWeapon(new LaserWeapon(*game, *player, weaponTexture, *textureSize, *weaponOffset, *projectileColorOffset, coolDown));
+    SDL_Texture* weaponTexture = TextureFactory::instance(w->getRenderer()).getTexture(
+            path + "/../images/laser_shot.png");
+    player->setWeapon(
+            new LaserWeapon(*game, *player, weaponTexture, *textureSize, *weaponOffset, *projectileColorOffset,
+                            coolDown));
 
     game->setPlayer(player);
     player->setFocus(true);
@@ -112,15 +114,15 @@ void setupGame(string filename, MainWindow* w, Game* game)
     player->setColorOffset(colorOffset);
 
 
-
-
     vector<XML::LevelBot> bots = xml.getLevelBots();
 
-    for (auto it = begin (bots); it != end (bots); it++)
+    for (auto it = begin(bots); it != end(bots); it++)
     {
-        texture       = TextureFactory::instance(w->getRenderer()).getTexture("/home/marius/git/CPPP/praktikum1/res/images/enemys_green_40x40.png");
+        texture = TextureFactory::instance(w->getRenderer()).getTexture(
+                "/home/marius/git/CPPP/praktikum1/res/images/enemys_green_40x40.png");
 
-        Bot* bot = new Bot(w->getRenderer(), texture,(*it).type.frameWidth, (*it).type.frameHeight, (*it).type.numFrames, (*it).type.npc);
+        Bot* bot = new Bot(w->getRenderer(), texture, (*it).type.frameWidth, (*it).type.frameHeight,
+                           (*it).type.numFrames, (*it).type.npc);
 
         getBotProperty(*it, p);
         bot->setPhysics(p);
@@ -257,20 +259,20 @@ void setupGame(string filename, MainWindow* w, Game* game)
 
 int main(int argc, char** argv)
 {
-	if(argc != 2)
-	{
-		std::cout << "Please specify a level to render" << std::endl;
-		return -1;
-	}
+    if (argc != 2)
+    {
+        std::cout << "Please specify a level to render" << std::endl;
+        return -1;
+    }
 
-	MainWindow window("Jumper", 625, 576);
-	Game game(&window);
-	setupGame(argv[1], &window, &game);
+    MainWindow window("Jumper", 625, 576);
+    Game game(&window);
+    setupGame(argv[1], &window, &game);
 
-	window.setGame(&game);
-	game.start();
-	window.run();
+    window.setGame(&game);
+    game.start();
+    window.run();
 
-	// Free textures
-	TextureFactory::instance(window.getRenderer()).deleteAll();
+    // Free textures
+    TextureFactory::instance(window.getRenderer()).deleteAll();
 }
