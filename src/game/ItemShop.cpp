6fd6857cdfb66f75e436582a107c8b5331d8a17a
kanashius <kanashius@gmail.com>
2016-02-29 16:27:18 +0100

    added xml

--- src/game/ItemShop.cpp
+++ src/game/ItemShop.cpp
@@ -4,3 +4,32 @@ ItemShop::ItemShop()
 {
 
 }
+
+void ItemShop::mainLoop(const Uint8*& currentKeyStates, const bool* keyDown){
+    if (currentKeyStates[SDL_SCANCODE_UP])
+    {
+        //key
+    }
+    //Clear Renderer
+    SDL_RenderClear(m_renderer);
+
+    /*for (size_t i = 0; i < m_renderables.size(); i++)
+    {
+        m_renderables[i]->render();
+    }*/
+
+    // Update screen
+    SDL_RenderPresent(m_renderer);
+}
+
+void ItemShop::payMoney(int money){
+
+}
+
+void ItemShop::loadMoney(){
+
+}
+
+void ItemShop::addMoney(int money){
+
+}
--- src/game/ItemShop.hpp
+++ src/game/ItemShop.hpp
@@ -1,11 +1,19 @@
 #ifndef ITEMSHOP_H
 #define ITEMSHOP_H
-
+#include "MainWindow.hpp"
+#include "SDL.h"
 
 class ItemShop
 {
     public:
         ItemShop();
+        void mainLoop(const Uint8*& currentKeyStates, const bool* keyDown);
+        static void addMoney(int money);
+    private:
+        void payMoney(int money);
+        void loadMoney();
+        std::string sPaymentFile;
+        SDL_Renderer* m_renderer;
 };
 
 #endif // ITEMSHOP_H
