1bd90750569d62e948ea3bf367252f6a837f5563
Patrick Steinforth <patrick@steinforth.eu>
2016-03-01 13:56:10 +0100

    move findFiles method from MainMenu to Filesystem

--- src/game/MainMenu.cpp
+++ src/game/MainMenu.cpp
@@ -6,43 +6,16 @@
 * @date 29 Feb 2016
 */
 #include "MainMenu.hpp"
+#include "Filesystem.hpp"
 
 namespace jumper
 {
-
-std::vector<fs::path> MainMenu::findFiles(const fs::path& path,
-                                          boost::regex pattern)
-{
-    std::vector<fs::path> vec;
-    if (!fs::is_directory(path)) //!path.empty()
-    {throw std::invalid_argument("You have to provide a resource DIRECTORY!");}
-
-        fs::directory_iterator end; //default construction yields past-the-end
-
-        for (fs::directory_iterator i(path); i != end; ++i, !path.empty())
-        {
-            // Skip if not a file
-            if( !boost::filesystem::is_regular_file( i->status() ) ) continue;
-
-            boost::smatch match_result;
-
-            // Skip if no match for V2:
-            //if( !boost::regex_match(i->leaf(), match_result, pattern ) ) continue;
-            // For V3:
-            if(!boost::regex_match(i->path().filename().string(), match_result, pattern)) continue;
-
-            // File matches, store it
-            vec.push_back( i->path().filename() );
-            //std::cout << i->path().filename().string() << std::endl; //Debug output
-        }
-    return vec;
-}
 
 
     MainMenu::MainMenu(MainWindow* win, Game* game, fs::path resDir) :
     m_win(win), m_resDir(resDir), m_game(game)
     {
-        m_levelFiles = findFiles(resDir, boost::regex("^.*\\.xml$"));
+        m_levelFiles = Filesystem::findFiles(resDir, boost::regex("^.*\\.xml$"));
     }
 
     void MainMenu::update(const Uint8*& currentKeyStates, const bool* keyDown)
--- src/game/MainMenu.hpp
+++ src/game/MainMenu.hpp
@@ -7,7 +7,6 @@
 */
 #ifndef SCROLLER_MAINMENU_HPP
 #define SCROLLER_MAINMENU_HPP
-
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/path.hpp>
 #include <boost/regex.hpp>
@@ -28,14 +27,6 @@ public:
     void update(const Uint8*& currentKeyStates, const bool* keyDown);
 
 private:
-    /**
-     * Searches files within a directory that match a regex pattern.
-     * @param path The directory path.
-     * @param pattern Regex pattern.
-     * @return All files within the given path that match the pattern.
-     */
-    std::vector<fs::path> findFiles(const fs::path& path,
-                                    boost::regex pattern);
 
     std::vector<fs::path> m_levelFiles;
     MainWindow* m_win;
