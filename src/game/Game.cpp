5ae5ed5cedbb5c8ddd22df7458ddbbc8b5e17ce1
Benjamin Lücking <bluecking@uos.de>
2016-03-02 16:27:32 +0100

    Set godmode powerup duration dynamically

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -231,7 +231,7 @@ namespace jumper
                         it->type.frameHeight,
                         it->type.numFrames);
 
-                powerUp->setExpirationTime(10);
+                powerUp->setExpirationTime(it->value);
             }
             else if (it->type.type.compare("RESTORE_HEALTH") == 0)
             {
