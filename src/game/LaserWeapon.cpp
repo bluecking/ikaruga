bad045e70fa1fb52142cd4c1d74249dabaf6c1a5
nwollek <nwollek@uni-osnabrueck.de>
2016-03-02 14:48:04 +0100

    Added spread factor and considering of default movement of projectiles

--- src/game/LaserWeapon.cpp
+++ src/game/LaserWeapon.cpp
@@ -17,17 +17,20 @@ namespace jumper
             return;
         }
 
-        // Pointer for created projectiles
-        Projectile* projectile;
-
         // Number of projectiles
-        int numProjectiles = 5; //getEvolutionStage();
+        int numProjectiles = getEvolutionStage();
+
+        // Scale of spread of projectiles growing with projectile height
+        float spreadScale = m_projectileTextureSize.y() * 0.005f;
 
         // Spawn position of projectiles
         Vector2f spawnPos = (spawnPosition + m_weaponOffset);
 
         // yOffset for even projectile number
         float yOffset = -(((numProjectiles + 1) % 2) * 0.5f);
+
+        // Pointer for created projectiles
+        Projectile* projectile;
 
         for(int i = 0; i < numProjectiles; i++)
         {
@@ -40,14 +43,11 @@ namespace jumper
                                         m_collisionDamage);
 
             // Calculate y-movement of current projectile
-            float moveY = ((numProjectiles / 2) - i + yOffset) * 0.02f;
-
-            //std::cout << "i: " << i << "| moveY: " << moveY << std::endl;
-            std::cout << direction.y() << std::endl;
+            float moveY = ((numProjectiles / 2) - i + yOffset) * spreadScale;
 
             // Set projectile properties
             projectile->setColorOffset(m_projectileColorOffset);
-            projectile->setDirection(Vector2f(0.25f, moveY));
+            projectile->setDirection(Vector2f(direction.x() * 0.3f, moveY * 0.3f));
             projectile->setType(ActorType::PROJECTILE);
             projectile->setPosition(spawnPos);
             projectile->setColor(m_actor.getColor());
