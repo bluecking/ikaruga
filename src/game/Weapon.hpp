49e2fe53ec8b7f07cd99d8fe8d7f75f98169ca39
Varaion <dennis@altenhoff-lgh.de>
2016-03-02 11:40:43 +0100

    Make weapons work!

--- src/game/Weapon.hpp
+++ src/game/Weapon.hpp
@@ -18,7 +18,14 @@ namespace jumper
     class Game;
 
     class Actor;
-
+    namespace WeaponType {
+        enum WeaponType {
+            LASER_GUN,
+            BLASTER,
+            ROCKET,
+            NONE
+        };
+    }
     /**
      * @brief Absract class for weapons. Handles basic functionality like weapon cool down.
      */
