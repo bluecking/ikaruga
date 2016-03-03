7178df7d30318e71d3f9a8475970c24cc4b0becd
Patrick Steinforth <patrick@steinforth.eu>
2016-03-03 17:59:17 +0100

    comment main.cpp

--- src/game/Main.cpp
+++ src/game/Main.cpp
@@ -1,3 +1,9 @@
+/**
+ * Main.cpp
+ *
+ * @date 03.03.16
+ * @author Patrick Steinforth (psteinforth@uni-osnabrueck.de)
+ */
 #include "MainWindow.hpp"
 #include "Filesystem.hpp"
 
@@ -6,17 +12,21 @@ using std::string;
 using std::cout;
 using std::endl;
 
-
+/**
+ * @brief Takes command line paramter and starts MainMenu
+ *
+ * @param argv command line parameters. You have to provide a game specific resource directory.
+ * @return exit code
+ */
 int main(int argc, char** argv)
 {
     if (argc != 2)
-
     {
         std::cout << "Invalid amount of parameters." << std::endl;
         return -1;
     }
 
-    MainWindow window("Ikaruga", 625, 576,boost::filesystem::path(argv[1]));
+    MainWindow window("Ikaruga", 625, 576, boost::filesystem::path(argv[1]));
 
     MainMenu menu(&window, boost::filesystem::path(argv[1]));
     window.setMenu(&menu);
