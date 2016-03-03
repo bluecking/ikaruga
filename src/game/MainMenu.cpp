1be4455fa10c7c4317d6e6b4bf1d4045e33ca322
Marius <mbrockmo@uos.de>
2016-03-03 15:05:03 +0100

    fixed

--- src/game/MainMenu.cpp
+++ src/game/MainMenu.cpp
@@ -77,10 +77,6 @@ namespace jumper
                 {
                     m_table.decrease();
                 }
-         //       m_table.setStringProperties(2, 1, 0, m_tableText);
-          //      m_table.render();
-           //     SDL_RenderPresent(m_win->getRenderer());
-
                 if(keyDown[SDL_SCANCODE_ESCAPE])
                 {
                     m_menu = MAIN_MENU;
